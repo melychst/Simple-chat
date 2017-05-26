64d070d62907ab164626bcfc7d778554190ff6e0
Stepan Melykh <melychst@gmail.com>
2017-05-26 18:10:10 +0300

    Init repository

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Chat</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1>Simple chat</h1>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
